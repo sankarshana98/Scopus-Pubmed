332a0982ddc4e6a149b10cce34e8a95f0365a329
Sankarshana Dorbala <52189722+sankarshana98@users.noreply.github.com>
2022-12-29 11:21:02 -0800

    Update scopus.py

--- scopus.py
+++ scopus.py
@@ -12,10 +12,9 @@ data = []
 
 
 def scopus_data(searchTerm):
-    apikey = '77ab6726b07dd68b819f8714a7bc0129'
+    apikey = 'Your API KEY'
     client = ElsClient(apikey)
     doc_srch = ElsSearch("KEY({}) AND PUBYEAR > 2021".format(searchTerm), 'scopus')
-    #print("doc_srch has", len(doc_srch.results))
     doc_srch.execute(client, get_all=True)
     return doc_srch.results
 
