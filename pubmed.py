44cece90ee420dc9cb457a95d5dd488e04663251
Sankarshana Dorbala <52189722+sankarshana98@users.noreply.github.com>
2022-12-29 11:20:10 -0800

    Update pubmed.py

--- pubmed.py
+++ pubmed.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 def search(query):
-    Entrez.email = 'akuzhiv1@binghmaton.edu'
+    Entrez.email = ' your mail registered with pubmed API'
     handle = Entrez.esearch(db='pubmed',
                             sort='relevance',
                             retmax=10_000,
@@ -18,7 +18,7 @@ def search(query):
 
 def fetch_details(id_list):
     ids = ','.join(id_list)
-    Entrez.email = 'akuzhiv1@binghmaton.edu'
+    Entrez.email = ' your mail registered with pubmed API'
     handle = Entrez.efetch(db='pubmed',retmode='xml',id=ids)
     results = Entrez.read(handle)
     return results
